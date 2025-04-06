bbf0eea9db0f2b34cc9277202413239fdbdcc7e6
ridwanbdr <ridwanbdr12@gmail.com>
2025-04-06 22:57:10 +0700

    Landing Page - Done

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// route to landing page
+Route::get('/landingpage', function () {
+    return view('LandingPage.landingpage');
+});
